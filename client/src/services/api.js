f34ecf96c2a30ddc6bb3aec25810bd03d9f17e6d
adithyagarapati <adithyavardhan93@gmail.com>
2025-03-01 01:48:57 +0530

    API Eror fix for client

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -28,4 +28,105 @@ api.interceptors.response.use(
   }
 );
 
+// User API functions
+export const getUsers = async () => {
+  const response = await api.get('/users');
+  return response.data;
+};
+
+export const getUserById = async (userId) => {
+  const response = await api.get(`/users/${userId}`);
+  return response.data;
+};
+
+export const updateUser = async (userId, userData) => {
+  const response = await api.put(`/users/${userId}`, userData);
+  return response.data;
+};
+
+// Exercise API functions
+export const getExercises = async () => {
+  const response = await api.get('/exercises');
+  return response.data;
+};
+
+export const getExerciseById = async (exerciseId) => {
+  const response = await api.get(`/exercises/${exerciseId}`);
+  return response.data;
+};
+
+export const createExercise = async (exerciseData) => {
+  const response = await api.post('/exercises', exerciseData);
+  return response.data;
+};
+
+export const updateExercise = async (exerciseId, exerciseData) => {
+  const response = await api.put(`/exercises/${exerciseId}`, exerciseData);
+  return response.data;
+};
+
+export const deleteExercise = async (exerciseId) => {
+  const response = await api.delete(`/exercises/${exerciseId}`);
+  return response.data;
+};
+
+// Workout API functions
+export const getWorkoutsByUser = async (userId) => {
+  const response = await api.get(`/workouts/user/${userId}`);
+  return response.data;
+};
+
+export const getWorkoutById = async (workoutId) => {
+  const response = await api.get(`/workouts/${workoutId}`);
+  return response.data;
+};
+
+export const createWorkout = async (workoutData) => {
+  const response = await api.post('/workouts', workoutData);
+  return response.data;
+};
+
+export const updateWorkout = async (workoutId, workoutData) => {
+  const response = await api.put(`/workouts/${workoutId}`, workoutData);
+  return response.data;
+};
+
+export const deleteWorkout = async (workoutId) => {
+  const response = await api.delete(`/workouts/${workoutId}`);
+  return response.data;
+};
+
+export const getMaxWeightsByUser = async (userId) => {
+  const response = await api.get(`/workouts/max-weights/${userId}`);
+  return response.data;
+};
+
+// Measurement API functions
+export const getMeasurementsByUser = async (userId) => {
+  const response = await api.get(`/measurements/user/${userId}`);
+  return response.data;
+};
+
+export const getMeasurementById = async (measurementId) => {
+  const response = await api.get(`/measurements/${measurementId}`);
+  return response.data;
+};
+
+export const createMeasurement = async (measurementData) => {
+  const response = await api.post('/measurements', measurementData);
+  return response.data;
+};
+
+export const updateMeasurement = async (measurementId, measurementData) => {
+  const response = await api.put(`/measurements/${measurementId}`, measurementData);
+  return response.data;
+};
+
+// Health check
+export const checkApiHealth = async () => {
+  const response = await api.get('/health');
+  return response.data;
+};
+
+// Default export for backward compatibility
 export default api;
